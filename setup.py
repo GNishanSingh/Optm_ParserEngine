041141d7b8e6d532bd742764b7930e9dd34b7053
Gurmukhnishan Singh <nishansaiyan@gmail.com>
2022-05-31 00:17:24 +0530

    addition of setup and module files

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Optm_ParserEngine',
+      version='1.0',
+      description='Python Regex Parser Engine',
+      author='Gurmukhnishan Singh',
+      author_email='info@gurmukhnishansingh.me',
+      packages=['OptmPE'],
+     )
